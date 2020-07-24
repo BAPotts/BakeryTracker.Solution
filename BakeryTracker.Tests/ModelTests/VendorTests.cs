aec309cafe4097647d76aff4c28597d6b58dc68d
Beverly Potts <pottsbeverly@gmail.com>
2020-07-24 16:59:29 -0400

    Fail ReturnsDescription test

--- BakeryTracker.Tests/ModelTests/VendorTests.cs
+++ BakeryTracker.Tests/ModelTests/VendorTests.cs
@@ -7,14 +7,25 @@ namespace BakeryTracker.Tests
 {
   [TestClass]
 
-  public class VendorTest 
+  public class VendorTests
   {
 
   [TestMethod]
     public void VendorConstructor_CreatesVendorObject_Vendor()
     {
-      Vendor newVendor = new Vendor();
+      Vendor newVendor = new Vendor("test string");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    { //Arrange
+      string description = "Pizza place in Royal Oak";
+      Vendor newVendor = new Vendor(description);
+      //Act
+      string result = newVendor.Description;
+      //Assert
+      Assert.AreEqual(description, result);
     }
   }
 }
--- BakeryTracker/Models/Vendor.cs
+++ BakeryTracker/Models/Vendor.cs
@@ -5,5 +5,11 @@ namespace BakeryTracker.Models
 
   public class Vendor
   {
+    public string Description {get;}
+
+    public Vendor(string description)
+    {description = "banana";
+      Description = description ;
+    }
   }
 }
