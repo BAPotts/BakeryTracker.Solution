6ef214db3d04cf407001855a49b81aeaf228aaa6
Beverly Potts <pottsbeverly@gmail.com>
2020-07-24 19:41:43 -0400

    Add ReturnsDate test

--- BakeryTracker.Tests/ModelTests/OrderTests.cs
+++ BakeryTracker.Tests/ModelTests/OrderTests.cs
@@ -14,6 +14,15 @@ namespace BakeryTracker.Tests
       Order newOrder = new Order();
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+    [TestMethod]
+    public void GetDate_ReturnsDate_String()
+    {
+      string date = "7/24/2020";
+      Order newOrder = newOrder(date);
+      string result = newOrder.Date;
+      Assert.AreEqual(date, result);
+    }
+
   }
 
 
