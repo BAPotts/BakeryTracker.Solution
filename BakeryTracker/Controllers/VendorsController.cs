e06c65841eb7cb49b22258bf421a0054d4ebfc08
Beverly Potts <pottsbeverly@gmail.com>
2020-08-02 14:44:01 -0400

    Add Show method to vendors controller

--- BakeryTracker/Controllers/VendorsController.cs
+++ BakeryTracker/Controllers/VendorsController.cs
@@ -25,6 +25,15 @@ namespace BakeryTracker.Controllers
       Vendor newVendor = new Vendor(vendDescrip, vendorName);
       return RedirectToAction("Index");
     }
-
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("Orders", vendorOrders);
+      return View(model);
+    }
   }   
 }    
